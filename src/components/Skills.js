5fe415146262d66e4af28a7e19bf0a1ba8a96a79
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-08-26 23:31:34 +0530

    update skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,80 +1,55 @@
 import React from "react";
 
-const backpack = [
-  {
-    name: "Go",
-    url: "https://img.icons8.com/color/144/000000/golang.png",
-  },
-  {
-    name: "JS",
-    url: "https://img.icons8.com/color/144/000000/javascript--v1.png",
-  },
-  {
-    name: "TS",
-    url: "https://img.icons8.com/color/144/000000/typescript.png",
-  },
-  {
-    name: "Cpp",
-    url: "https://img.icons8.com/color/144/000000/c-plus-plus-logo.png",
-  },
-  {
-    name: "Python",
-    url: "https://img.icons8.com/color/144/000000/python--v1.pn",
-  },
-  {
-    name: "html",
-    url: "https://img.icons8.com/color/144/000000/html-5--v2.png",
-  },
-  {
-    name: "node-js",
-    url: "https://img.icons8.com/color/144/000000/nodejs.png",
-  },
-  {
-    name: "buffalo-go",
-    url: "https://gobuffalo.io/assets/images/logo_med.png",
-  },
-  {
-    name: "react",
-    url: "https://img.icons8.com/color/144/000000/react-native.png",
-  },
-  {
-    name: "mongoDB",
-    url: "https://img.icons8.com/color/144/000000/mongodb.png",
-  },
-  {
-    name: "postgreesql",
-    url: "https://img.icons8.com/color/144/000000/postgreesql.png",
-  },
-  {
-    name: "redis",
-    url: "https://img.icons8.com/color/144/000000/redis.png",
-  },
-  {
-    name: "docker",
-    url: "https://img.icons8.com/fluency/144/000000/docker.png",
-  },
-  {
-    name: "linux",
-    url: "https://img.icons8.com/color/144/000000/linux--v1.png",
-  },
+const languages = [
+  "C++",
+  "Go",
+  "JavaScript",
+  "TypeScript",
+  "Python",
+  "HTML",
+  "CSS",
 ];
+
+const frameworks = [
+  "FastAPI",
+  "NodeJS",
+  "ExpressJS",
+  "React",
+  "React Native",
+  "Redux",
+  "MobX",
+  "Tailwind CSS",
+  "Bootstrap",
+];
+
+const tools = ["Postgresql", "MongoDB", "Redis", "Docker", "Linux"];
+
+const SkillSection = ({ titile, list }) => {
+  return (
+    <div className="pb-5 xl:grid xl:place-items-center">
+      <h1 className="text-3xl pb-3 font-black text-white">{titile}</h1>
+      <div className="flex flex-wrap">
+        {list.map((value, id) => (
+          <div
+            key={id}
+            className="h-10 m-3 grid place-content-center rounded-lg bg-white transform hover:scale-110"
+          >
+            <p className=" p-3 text-gray-800 text-md font-semibold">{value}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Skills = () => {
   return (
-    <div className="h-screen pt-10 pl-6 md:pl-16">
-      <h1 className="text-5xl font-semibold pb-10">
-        {" "}
-        My Development Backpack{" "}
-      </h1>
-      <div className="xl:w-3/5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 ">
-        {backpack.map(({ url, name }, id) => (
-          <img
-            className="w-32 h-32 hover:animate-pulse"
-            key={id}
-            src={url}
-            alt={name}
-          />
-        ))}
+    <div className="bg-blue-400 flex justify-center items-center py-10">
+      <div className="w-full xl:w-3/5 xl:grid xl:place-items-center lg:px-12 px-6 xl:px-0">
+        <h1 className="text-6xl text-yellow-300 font-black pb-6">Skills</h1>{" "}
+        <SkillSection titile="Programming Languages" list={languages} />
+        <SkillSection titile="Framework / libraries" list={frameworks} />
+        <SkillSection titile="Tools" list={tools} />
       </div>
     </div>
   );
