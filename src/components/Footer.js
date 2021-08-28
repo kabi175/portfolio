9c0dffbff79ba1b71f50b0274e1e6901aa7cc83c
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-08-29 00:25:25 +0530

    add projects

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="h-60 w-full bg-gray-600 text-white grid place-items-center  py-16">
+      <span className="font-black text-xl"> © Kabilan Muthusamy </span>
+      <span className="font-black text-xl">
+        Build with <span className="text-blue-400"> React </span> & ☕️{" "}
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Icons } from "../data";
+
+function ProjectCard({ title, description, language, repoUrl, demoUrl }) {
+  return (
+    <div className="w-96 boerder border-2 border-gray-200 rounded mx-7 my-5 p-5  shadow-xl flex flex-col flex-nowrap transform hover:scale-125">
+      <h1 className="self-start text-2xl font-bold ">{title}</h1>
+      <p className="text-gray-600 py-4">{description}</p>
+      <span className={Icons[language] | ""} />
+      <div className="w-full grid grid-cols-2">
+        <a
+          href={repoUrl}
+          target="_blank"
+          rel="noreferrer"
+          className="place-self-start text-lg text-indigo-800 font-bold"
+        >
+          {"📁 CODE"}
+        </a>
+        <a
+          href={demoUrl}
+          target="_blank"
+          rel="noreferrer"
+          className={`${
+            demoUrl ? "place-self-end" : "hidden"
+          } text-lg text-indigo-800 font-bold`}
+        >
+          {" "}
+          {"📺 DEMO"}{" "}
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
