9180c44acaaaf479ebe19d164b672532ffc834e5
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-26 15:07:47 +0530

    removed tags

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,35 +1,40 @@
-import React from "react";
-import { languages, frameworks, tools } from "../data";
+import React from 'react';
+import { languages, frameworks, tools } from '../data';
 
 const SkillSection = ({ titile, list }) => {
-  return (
-    <div className="pb-5 xl:grid xl:place-items-center">
-      <h1 className="text-3xl pb-3 font-black text-white">{titile}</h1>
-      <div className="flex flex-wrap justify-center">
-        {list.map((value, id) => (
-          <div
-            key={id}
-            className="h-10 m-3 grid place-content-center rounded-lg bg-white transform hover:scale-110"
-          >
-            <p className=" p-3 text-gray-800 text-md font-semibold">{value}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='pb-10 xl:grid xl:place-items-center'>
+			<div className='flex flex-wrap justify-center'>
+				{list.map((value, id) => (
+					<div
+						key={id}
+						className='h-10 m-3 grid place-content-center rounded-lg bg-white transform hover:scale-110'
+					>
+						<p className=' p-3 text-gray-700 text-md font-semibold'>
+							{value}
+						</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 const Skills = () => {
-  return (
-    <div className="bg-blue-400 flex justify-center items-center py-10">
-      <div className="w-full xl:w-3/5 xl:grid xl:place-items-center lg:px-12 px-6 xl:px-0">
-        <h1 className="text-6xl text-yellow-300 font-black pb-6">Skills</h1>{" "}
-        <SkillSection titile="Programming Languages" list={languages} />
-        <SkillSection titile="Framework / libraries" list={frameworks} />
-        <SkillSection titile="Tools" list={tools} />
-      </div>
-    </div>
-  );
+	return (
+		<div className='bg-cloud-blue flex justify-center items-center py-10'>
+			<div className='w-full xl:w-3/5 grid place-items-center lg:px-12 px-6 xl:px-0'>
+				<h1 className=' text-5xl text-white font-black pb-6 flex'>
+					My Tech Backpack{' '}
+					<span className='hidden md:flex pl-3'> 🎒 </span>
+				</h1>{' '}
+				<SkillSection
+					titile='Programming Languages'
+					list={[...languages, ...frameworks, ...tools]}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Skills;
