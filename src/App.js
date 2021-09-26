e2b556353bb027ce6ee6e4738b80f0708c4e55b2
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-26 15:09:09 +0530

    added About

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="font-mono">
-      {" "}
-      <About /> <Skills /> <Projects /> <Footer />
-    </div>
-  );
+	return (
+		<div className='font-mono'>
+			<Home /> <About /> <Skills /> <Projects /> <Footer />
+		</div>
+	);
 }
 
 export default App;
