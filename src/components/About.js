ce9737874109335850de6e7d30b048d1615c3102
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-27 23:24:50 +0530

    added ref

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Icons from './Icons';
 
-export default function About() {
+export default function About({ Ref }) {
 	return (
-		<div className='bg-white py-10 px-5  flex flex-col justify-center items-center'>
-			<h1 className='text-5xl font-bold'>Hello World! 👋</h1>
-			<div className='py-5 text-xl text-gray-600  xl:w-4/6'>
-				<p className='py-5'>
+		<div
+			ref={Ref}
+			tabIndex='-1'
+			className='bg-white pt-10 px-3  flex flex-col justify-center items-center text-lg'
+		>
+			<h1 className='text-4xl md:text-5xl font-bold'>Hello World! 👋</h1>
+			<div className='py-5 md:text-4xl text-gray-600  xl:w-4/6'>
+				<p className='py-5 px-3'>
 					My name is kabilan. I'm a Student who is passionate about
 					software development and competitive programming.
 				</p>
-				<p className='py-5'>
+				<p className='py-5 px-3'>
 					I enjoy learning new technologies in the ever-changing
 					computer science field and thrive to build upon my existing
 					knowledge with experience.
 				</p>
-				<p className='py-5'>
+				<p className='py-5 px-3'>
 					Get in touch at{' '}
 					<a
 						href='mailto:kabilan-muthusamy@outlook.com'
@@ -26,8 +30,8 @@ export default function About() {
 						kabilan-muthusamy@outlook.com
 					</a>
 				</p>
+				<Icons />
 			</div>
-			<Icons />
 		</div>
 	);
 }
