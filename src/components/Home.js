0dc2a207df756e95675a5334b99f1c0d848356dd
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-27 23:25:15 +0530

    updated bg-img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import TypingEffect from './TypingEffect';
 
-const About = () => {
+const About = ({ onFocusAbout }) => {
 	return (
-		<div className='h-screen bg-white flex flex-col justify-center items-center font-semibold text-white bg-cloud-blue'>
+		<div className='h-screen bg-white bg-cloud bg-no-repeat bg-top bg-contain md:bg-center flex flex-col justify-center items-center font-semibold text-white bg-cloud-blue'>
 			<div className='flex-grow flex items-center'>
 				<div className='grid gap-y-5 place-items-center xl:gap-y-10 md:gap-y-8'>
-					<img
-						style={{ position: 'absolute', zIndex: 0 }}
-						className='hidden sm:flex'
-						src='clouds.png'
-						alt='cloud'
-					/>
 					<img style={{ zIndex: 1 }} src='plane.gif' alt='plane' />
 
 					<h1
@@ -22,13 +16,16 @@ const About = () => {
 					</h1>
 					<h1
 						style={{ zIndex: 1 }}
-						className='font-normal text-xl xl:text-5xl md:text-4xl font-thin'
+						className='font-normal text-xl md:text-4xl xl:text-5xl  font-thin'
 					>
 						<TypingEffect />
 					</h1>
 				</div>
 			</div>
-			<span className=' text-3xl animate-bounce fontawesome-download' />
+			<span
+				className='hidden md:flex text-3xl animate-bounce fontawesome-download'
+				onClick={onFocusAbout}
+			/>
 		</div>
 	);
 };
