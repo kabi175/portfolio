99b55d89be2e6fc49f8d7298c2b63644f946bb54
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-09-27 23:25:01 +0530

    updated font size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
-  return (
-    <div className="h-60 w-full bg-gray-600 text-white grid place-items-center  py-16">
-      <span className="font-black text-xl"> © Kabilan Muthusamy </span>
-      <span className="font-black text-xl">
-        Build with <span className="text-blue-400"> React </span> & ☕️{" "}
-      </span>
-    </div>
-  );
+	return (
+		<div className='h-60 w-full bg-gray-600 text-white grid place-items-center  py-16 font-black md:text-xl'>
+			<span> © Kabilan Muthusamy </span>
+			<span>
+				Build with <span className='text-blue-400'> React </span> & ☕️{' '}
+			</span>
+		</div>
+	);
 }
 
 export default Footer;
